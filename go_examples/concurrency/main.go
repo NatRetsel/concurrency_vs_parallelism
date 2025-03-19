1cba064355773ee29f6eb393056816b33b153bbf
NatRetsel <natretseltries@gmail.com>
2025-03-19 10:41:31 +0800

    go concurrency example

--- go_examples/concurrency/main.go
+++ go_examples/concurrency/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -12,28 +13,58 @@ func sayHello(goroutineNum string) {
 	}
 }
 
+func sendData(ch chan<- string, data string) {
+	fmt.Println("Sending data: ", data)
+	ch <- data // blocks until data is sent
+	fmt.Println("Finished sending: ", data)
+}
+
+func receiveData(ch <-chan string) {
+	data := <-ch // blocks until data is received
+	fmt.Println("Received: ", data)
+}
+
+type sharedObj struct {
+	mu    sync.Mutex
+	count int
+}
+
+func (so *sharedObj) increment() {
+	so.mu.Lock()
+	so.count++
+	fmt.Printf("Count: %v\n", so.count)
+	so.mu.Unlock()
+}
+
 func main() {
 	fmt.Println("==========Single Print go routine example==============")
-
 	go sayHello("goroutine 1") // Launches sayHello in a goroutine
 	go sayHello("goroutine 2")
 	fmt.Println("Hello from main!")
 	time.Sleep(time.Second * 2)
 	fmt.Println("=======================================================")
 
-	fmt.Println("==========Print from channel go routine example==============")
-	ch := make(chan string)
-	go func() {
-		ch <- "Hello from goroutine channel!"
-		close(ch)
-	}()
+	fmt.Println("=======Unbuffered Channel go routine example===========")
+	ch := make(chan string) // Unbuffered channel
+	go sendData(ch, "dummy data")
+	go receiveData(ch)
+	time.Sleep(time.Second)
+	fmt.Println("=======================================================")
 
-	msg, ok := <-ch
-	if !ok {
-		fmt.Println("Channel closed")
-	} else {
-		fmt.Printf("%v\n", msg)
+	fmt.Println("===================Mutex example=======================")
+	so := sharedObj{
+		count: 0, // count initialized to zero value
 	}
 
-	fmt.Println("=============================================================")
+	// anonymous function
+
+	for i := 0; i < 1000; i++ {
+		go func() {
+			so.increment()
+			time.Sleep(time.Millisecond * 100)
+		}()
+	}
+	time.Sleep(time.Second)
+	fmt.Printf("Final count: %v\n", so.count)
+	fmt.Println("=======================================================")
 }
